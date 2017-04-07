f5db658ceba4f2c395896f47329839472f34193b
Mark Fine <mark.fine@gmail.com>
2017-04-07 16:36:37 -0700

    relax the constraints.

--- src/Preamble/Stats.hs
+++ src/Preamble/Stats.hs
@@ -22,23 +22,23 @@ sendto metric = do
   a <- view scSockAddr
   liftIO $ void $ sendTo s metric a
 
-stats :: (MonadStatsCtx c m, Num a, Show a) => Text -> Text -> a -> Tags -> m ()
+stats :: (MonadStatsCtx c m, Show a) => Text -> Text -> a -> Tags -> m ()
 stats stat name value tags = do
   let metric = name -:- show value -|- stat
       tagged = intercalate "," $ flip map tags $ uncurry (-:-)
   sendto $ encodeUtf8 $ bool (metric -|- "#" <> tagged) metric $ null tags
 
-statsCount :: (MonadStatsCtx c m, Num a, Show a) => Text -> a -> Tags -> m ()
+statsCount :: (MonadStatsCtx c m, Show a) => Text -> a -> Tags -> m ()
 statsCount = stats "c"
 
-statsGauge :: (MonadStatsCtx c m, Num a, Show a) => Text -> a -> Tags -> m ()
+statsGauge :: (MonadStatsCtx c m, Show a) => Text -> a -> Tags -> m ()
 statsGauge = stats "g"
 
-statsHistogram :: (MonadStatsCtx c m, Num a, Show a) => Text -> a -> Tags -> m ()
+statsHistogram :: (MonadStatsCtx c m, Show a) => Text -> a -> Tags -> m ()
 statsHistogram = stats "h"
 
-statsTimer :: (MonadStatsCtx c m, Num a, Show a) => Text -> a -> Tags -> m ()
+statsTimer :: (MonadStatsCtx c m, Show a) => Text -> a -> Tags -> m ()
 statsTimer = stats "ms"
 
-statsSet :: (MonadStatsCtx c m, Num a, Show a) => Text -> a -> Tags -> m ()
+statsSet :: (MonadStatsCtx c m, Show a) => Text -> a -> Tags -> m ()
 statsSet = stats "s"
